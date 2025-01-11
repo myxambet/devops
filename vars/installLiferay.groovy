9655e8b5c46cebc240067d7f5676183ca40d9e65
myxambet <52917808+myxambet@users.noreply.github.com>
2025-01-12 01:49:29 +0600

    Update installLiferay.groovy

--- vars/installLiferay.groovy
+++ vars/installLiferay.groovy
@@ -1,12 +1,13 @@
 // vars/installLiferay.groovy
-def installLiferay(String serverIp, String liferayLogin, String liferayPassword) {
-    echo "Starting Liferay installation on server: ${serverIp}"
+def installLiferay() {
+   
     
     sh """
-    wsl ansible-playbook -i ${serverIp}, -u ${liferayLogin} --extra-vars 'ansible_ssh_pass=${liferayPassword}' install_liferay.yml
-    """
+     wsl ansible-playbook -i inventory/host.ini playbook.yml  --ask-vault-password
+     """
     
-    echo "Liferay installation completed on server: ${serverIp}"
+    
+  
 }
 
 }
