68b68718d740ea7fd20df28d1b6b00afed7b852d
myxambet <52917808+myxambet@users.noreply.github.com>
2025-01-12 02:06:03 +0600

    Update installLiferay.groovy

--- vars/installLiferay.groovy
+++ vars/installLiferay.groovy
@@ -3,7 +3,7 @@ def call() {
    
     
     bat """
-     ansible-playbook -i /home/muha1022/liferayPostgres_install/inventory/host.ini /home/muha1022/liferayPostgres_install/playbook.yml --ask-vault-password
+     wsl ansible-playbook -i /home/muha1022/liferayPostgres_install/inventory/host.ini /home/muha1022/liferayPostgres_install/playbook.yml --ask-vault-password
      """
     
     
