cbdc60db5eb78f10763189a27b27046ba4c26320
myxambet <52917808+myxambet@users.noreply.github.com>
2025-01-13 23:37:43 +0600

    Update installLiferay.groovy

--- vars/installLiferay.groovy
+++ vars/installLiferay.groovy
@@ -1,9 +1,10 @@
 // vars/installLiferay.groovy
 def installLiferay(String serverIp, String liferayLogin, String liferayPassword) {
 
-    sh "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i ${serverIp}, /home/muha1022/liferayPostgres_install/liferay_inst.yml --vault-password-file=/home/muha1022/liferayPostgres_install/p_secrets.yml"
+  // sh "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i ${serverIp}, /home/muha1022/liferayPostgres_install/liferay_inst.yml --vault-password-file=/home/muha1022/liferayPostgres_install/p_secrets.yml"
     echo "$liferayLogin"
     echo "$liferayPassword"    
+    echo $serverIp
     
     
   
