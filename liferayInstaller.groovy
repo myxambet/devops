7b545b21ae20c2bd3f2e3237bb260479ad01223f
myxambet <52917808+myxambet@users.noreply.github.com>
2025-07-04 00:13:12 +0600

    Update liferayInstaller.groovy

--- liferayInstaller.groovy
+++ liferayInstaller.groovy
@@ -1,10 +1,6 @@
 pipeline {
     agent any
 
-    parameters {
-        string(name: 'GREETING', defaultValue: 'Привет, мир!', description: 'Введите приветствие')
-    }
-
     stages {
         stage('Показать приветствие') {
             steps {
