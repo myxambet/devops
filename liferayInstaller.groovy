bf11cac628b0e2d42abbe9a4c69ebc6a9677cf3e
myxambet <52917808+myxambet@users.noreply.github.com>
2025-07-27 00:16:50 +0600

    Update liferayInstaller.groovy

--- liferayInstaller.groovy
+++ liferayInstaller.groovy
@@ -1,32 +1,33 @@
 pipeline {
     agent any
 
-  properties([
-  parameters([
-    [$class: 'ChoiceParameter',
-      name: 'Environment',
-      description: 'Select target environment',
-      choiceType: 'PT_SINGLE_SELECT',
-      filterLength: 1,
-      filterable: false,
-      script: [
-        $class: 'GroovyScript',
-        script: [
-          sandbox: true,
-          classpath: [],
-          script: '''
-            return ["dev", "staging", "production"]
-          '''
+    // Объявляем параметр Environment
+    properties([
+      parameters([
+        [$class: 'ChoiceParameter',
+          name: 'Environment',
+          description: 'Select target environment',
+          choiceType: 'PT_SINGLE_SELECT',
+          filterLength: 1,
+          filterable: false,
+          script: [
+            $class: 'GroovyScript',
+            script: [
+              sandbox: true,
+              classpath: [],
+              script: '''
+                return ["dev", "staging", "production"]
+              '''
+            ]
+          ]
         ]
-      ]
-    ]
-  ])
-])
+      ])
+    ])
 
     stages {
-        stage('Показать приветствие') {
+        stage('Показать выбранное окружение') {
             steps {
-                echo "Значение параметра GREETING: ${params.GREETING}"
+                echo "Вы выбрали окружение: ${params.Environment}"
             }
         }
     }
