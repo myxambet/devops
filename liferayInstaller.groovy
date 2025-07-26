ec81672862ce5aaa82ff70227c9a0fc7d8c24229
myxambet <52917808+myxambet@users.noreply.github.com>
2025-07-27 01:07:21 +0600

    kljkl

--- liferayInstaller.groovy
+++ liferayInstaller.groovy
@@ -14,7 +14,7 @@ string(name: 'DEPLOY_ENV', defaultValue: params.DEPLOY_ENV ?:'staging', descript
           sandbox: true,
           classpath: [],
           script: '''
-            return  params.Environment ?:["dev", "staging", "production"]  
+            return  Environment ?:["dev", "staging", "production"]  
           '''
         ]
       ]
